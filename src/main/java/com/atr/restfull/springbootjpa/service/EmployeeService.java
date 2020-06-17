1cb2d5edc567441afab3d5667498fbba923642f8
Ahmed Al-Salih <1426391@delhaize.com>
2020-06-17 05:10:00 -0400

    fix the case latters issue by using criteriaBuilder.lower

---
+++ src/main/java/com/atr/restfull/springbootjpa/service/EmployeeService.java
@@ -0,0 +1,75 @@
+package com.atr.restfull.springbootjpa.service;
+
+import java.util.List;
+import java.util.Objects;
+
+import javax.persistence.criteria.Predicate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import com.atr.restfull.springbootjpa.model.Employee;
+import com.atr.restfull.springbootjpa.repository.EmployeeRepository;
+
+@Service
+public class EmployeeService {
+
+	@Autowired
+	EmployeeRepository employeeRepository;
+
+	public List<Employee> getSpecificationEmployee(String id, String firstName, String lastName, Pageable pageable) {
+
+		List<Employee> employees = employeeRepository.findAll((Specification<Employee>) (root, query, criteriaBuilder) -> {
+
+			Predicate predicate = criteriaBuilder.conjunction();
+			if (Objects.nonNull(id)) {
+				predicate = criteriaBuilder.and(predicate, criteriaBuilder.equal(root.get("id"), id));
+			}
+			if (!StringUtils.isEmpty(firstName)) {
+				predicate = criteriaBuilder.and(predicate, 
+						criteriaBuilder.like(criteriaBuilder.lower(root.get("firstName")), "%" + firstName.toLowerCase() + "%"));
+			}
+			if (!StringUtils.isEmpty(lastName)) {
+				predicate = criteriaBuilder.and(predicate,
+						criteriaBuilder.like(root.get("lastName"), "%" + lastName.toLowerCase() + "%"));
+			}
+			query.orderBy(criteriaBuilder.desc(root.get("firstName")), criteriaBuilder.asc(root.get("id")));
+
+			return predicate;
+		}, pageable).getContent();
+		return employees;
+
+	}
+	/**
+	 * public List<Employee> getSpecificationEmployee(String id, String firstName, String lastName, Pageable pageable) {
+
+		List<Employee> employees = employeeRepository.findAll(new Specification<Employee>() {
+
+			@Override
+			public Predicate toPredicate(Root<Employee> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
+
+				Predicate predicate = criteriaBuilder.conjunction();
+				if (Objects.nonNull(id)) {
+					predicate = criteriaBuilder.and(predicate, criteriaBuilder.equal(root.get("id"), id));
+				}
+				if (StringUtils.isEmpty(firstName)) {
+					predicate = criteriaBuilder.and(predicate, criteriaBuilder.like(root.get("firstName"), firstName));
+				}
+				if (StringUtils.isEmpty(lastName)) {
+					predicate = criteriaBuilder.and(predicate,
+							criteriaBuilder.like(root.get("lastName"), "%" + lastName + "%"));
+				}
+				query.orderBy(criteriaBuilder.desc(root.get("firstName")), criteriaBuilder.asc(root.get("id")));
+
+				return predicate;
+			}
+		}, pageable).getContent();
+		return employees;
+
+	}
+	 */
+
+}
