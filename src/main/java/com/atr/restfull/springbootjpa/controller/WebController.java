146e1a1224c6b5d8424c2203b6238dc3121d54df
Ahmed Al Salih <a.alsalih2@gmail.com>
2020-09-20 01:46:36 -0400

    change the create method return from void to ResponseEntit(body , headers , HTTPS.Status)

--- src/main/java/com/atr/restfull/springbootjpa/controller/WebController.java
+++ src/main/java/com/atr/restfull/springbootjpa/controller/WebController.java
@@ -6,6 +6,9 @@ import java.util.Optional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -46,8 +49,11 @@ public class WebController {
 	}
 	
 	@PostMapping("/employee")
-	public void createEmployee(@RequestBody @Valid Employee em) {
-		employeeRepository.save(em);
+	public ResponseEntity<Employee> createEmployee(@RequestBody @Valid Employee em) {
+		Employee createdEmployee = employeeRepository.save(em);
+		 HttpHeaders header = new HttpHeaders();
+		 header.set("MyResponseHeader", "MyValue");
+		return new ResponseEntity<Employee>(createdEmployee, header, HttpStatus.CREATED);
 	}
 	
 	@DeleteMapping("/employee/{id}")
